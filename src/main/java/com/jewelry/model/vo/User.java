9028af6ae3353814e61f5683d828968a8ffcc920
zhoutengyu <939557631@qq.com>
2018-10-30 10:29:02 +0800

    学习

--- src/main/java/com/jewelry/model/vo/User.java
+++ src/main/java/com/jewelry/model/vo/User.java
@@ -9,6 +9,29 @@ public class User implements Serializable {
     private String name;
     private String dengluming;
     private String password;
+    private int age;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+
+        User user = (User) o;
+
+        if (getAge() != user.getAge()) return false;
+        if (!getName().equals(user.getName())) return false;
+        if (!getDengluming().equals(user.getDengluming())) return false;
+        return getPassword().equals(user.getPassword());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getName().hashCode();
+        result = 31 * result + getDengluming().hashCode();
+        result = 31 * result + getPassword().hashCode();
+        result = 31 * result + getAge();
+        return result;
+    }
 
     public String getName() {
         return name;
@@ -32,5 +55,13 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
--- src/test/java/com/jewelry/JewelryApplicationTests.java
+++ src/test/java/com/jewelry/JewelryApplicationTests.java
@@ -5,12 +5,20 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class JewelryApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		//LinkedHashMap;
+		Map<String,Object> map=new HashMap<>();
+		map.put(null,"ss");
+		System.out.println(map.get(null));
 	}
 
 }
