f5e85ae50e81e970d99bf95f5c29882c7f0b4cf8
zhoutengyu <939557631@qq.com>
2019-03-06 16:40:44 +0800

    学习

---
+++ src/main/java/com/jewelry/test/TestParallel.java
@@ -0,0 +1,22 @@
+package com.jewelry.test;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class TestParallel {
+
+   /* public static void main(String[] args) {
+
+
+        int n=100000;
+        double fraction=1/n;
+        IntStream.range(0,n).parallel().mapToLong(twoDiceThrows());
+    }
+    private static int twoDiceThrows(){
+        int a=new Random().nextInt(6);
+        int b=new Random().nextInt(6);
+        return a+b+2;
+    }*/
+
+}
--- src/main/java/com/jewelry/test/TestStream.java
+++ src/main/java/com/jewelry/test/TestStream.java
@@ -15,6 +15,12 @@ public class TestStream {
         User u2=   new User("JackZhou", 22,"女");
         User u3=   new User("Tom", 20,"女");
         User u4=   new User("Bob", 60,"女");
+        List<User> c1=new ArrayList<>();
+        c1.add(u1);
+        List<User> c2=new ArrayList<>();
+        c2.add(u2);
+        c2.add(u3);
+        c2.add(u4);
         List<User> users=new ArrayList<>();
         users.add(u1);
         users.add(u2);
@@ -45,13 +51,27 @@ public class TestStream {
        Map<Boolean,List<User>> map= users.stream().collect(partitioningBy(x->x.getAge()>22));
        // map.get(false).forEach(x-> System.out.println(x.getAge()));
        Map<String,List<User>> map1= users.stream().collect(groupingBy(x->x.getSex()));
+        Map<String,List<String>> names=users.stream().collect(Collectors.groupingBy(x->x.getSex(),mapping(y->y.getName(),toList())));
+        users.stream().collect(Collectors.groupingBy(User::getSex,mapping(User::getName,toList())));
+
         Map<String,Long> s= users.stream().collect(groupingBy(x->x.getSex(),counting()));
         System.out.println(users.stream().collect(groupingBy(x->x.getSex(),counting())).get("男"));
-        
-
        //map1.get("女").forEach(x-> System.out.println(x.getName()));
         String str=users.stream().map(User::getName).collect(joining("-","[","}"));
         //System.out.println(str);
+        //遍历map，统计各个班级多少学生
+       Map<String,List<User>> sc=new HashMap<>();
+       sc.put("一班",c1);
+       sc.put("二班",c2);
+       Map<String,Integer> m=new HashMap<>();
+       sc.forEach((grade,students)->{
+           m.put(grade,students.size());
+       });
+        //System.out.println(m.toString());
+        //peak操作
+       users.stream().filter(u->u.getAge()>22).peek(x-> System.out.println(x.getAge())).map(x->x.getName()).collect(toList());
+
+        int g=1;
 
 
 
