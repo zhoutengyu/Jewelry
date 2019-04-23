f94a895a87dd9d47b9536948efddfa608789f676
zhoutengyu <939557631@qq.com>
2019-04-23 16:50:23 +0800

    学习

--- src/main/java/com/jewelry/threadDemo1/d2/Test.java
+++ src/main/java/com/jewelry/threadDemo1/d2/Test.java
@@ -1,10 +1,15 @@
 package com.jewelry.threadDemo1.d2;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by admin on 2019/4/11.
  */
 public class Test {
     public static void main(String[] args) {
-        int a = Global.r;
+        //int a = Global.r;
+        SimpleDateFormat sdf = new SimpleDateFormat("MMddHHmm");
+        System.out.println(sdf.format(new Date()));
     }
 }
